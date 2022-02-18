19a859953f5e71327504ffd0e494379484a887b7
User <masta54@mail.ru>
2022-02-18 19:26:15 +0700

    test comlite v2

--- src/test/java/ActionsTests.java
+++ src/test/java/ActionsTests.java
@@ -1,19 +1,20 @@
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-public class DropAndDropTests {
+public class ActionsTests {
 
+    //Действие с помощью action() не совершается
     @Test
     void successTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("div#column-a").$("header").shouldHave(text("A"));
         $("div#column-b").$("header").shouldHave(text("B"));
-        $("div#column-a").dragAndDropTo($("div#column-b"));
+        SelenideElement element = $("div#column-a");
+        actions().dragAndDropBy(element, 150, 0).perform();
         $("div#column-a").$("header").shouldHave(text("B"));
         $("div#column-b").$("header").shouldHave(text("A"));
-
     }
 }
