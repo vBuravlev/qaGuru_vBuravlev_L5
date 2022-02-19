e87de7480e15fb337f2f06946297bd27d89e15ac
User <masta54@mail.ru>
2022-02-20 00:21:26 +0700

    test complete v6

--- src/test/java/SearchDocSoftAssertionTests.java
+++ src/test/java/SearchDocSoftAssertionTests.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -12,12 +13,13 @@ public class SearchDocSoftAssertionTests {
     @Test
     void successTest() {
         open("https://github.com/selenide/selenide");
+        $("[itemprop=name]").shouldHave(text("selenide"));
         $("ul.UnderlineNav-body").$(byText("Wiki")).click(); // проверка ссылки Wiki на странице доки Selenide
-        $("a.internal.present[href*='SoftAssertions']").shouldHave(text("Soft assertions")).click(); //наличие ссылки SoftAssertion на страницу
+        $("div.markdown-body ul li a.internal.present[href*='SoftAssertions']").shouldHave(text("Soft assertions")).click(); //наличие ссылки SoftAssertion на страницу
         $("h3 a#user-content-example").parent().shouldHave(text("Example:"));  // наличие фразы "Example" на странице SoftAssertion
 
         ElementsCollection lines = $$("div.markdown-body ol"); //Поиск номера примера для JUnit5
-        for (int i = 1; i < lines.size(); i++) {
+        for (int i = 0; i < lines.size(); i++) {
             String expectedValue = lines.get(i).$("li").text();
             String actualValue = "Using JUnit5 extend test class:";
             if (expectedValue.equals(actualValue)) {
@@ -25,10 +27,8 @@ public class SearchDocSoftAssertionTests {
             }
         }
         // проверка блока с кодом на наличие специфических фраз
-        $$("ol[start='" + keyJunitNumber + "'] + div pre span").findBy(text("SoftAssertsExtension"))
-                .shouldBe(visible);
-        $$("ol[start='" + keyJunitNumber + "'] + div pre span").findBy(text("@Test"))
-                .shouldBe(visible);
+        $$("div.markdown-body ol").get(keyJunitNumber).sibling(0).shouldHave(text("SoftAssertsExtension"), text("@Test")); //проверка на специфичекские слова для JUnit 5
+
 
 
     }
